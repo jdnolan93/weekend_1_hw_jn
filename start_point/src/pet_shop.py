14e02fcf8e16d98ac50d99968a123063e4c128e4
Jamie <jamienolan93@hotmail.co.uk>
2021-11-21 14:38:38 +0000

    skipped a couple for now

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -4,6 +4,21 @@ def get_pet_shop_name(shop):
 def get_total_cash(total_cash):
     return total_cash["admin"]["total_cash"]
 
+# def add_or_remove_cash(New_cash, cash):
+#     New_cash = get_total_cash + 10
+#     return cash
+
+# def add_or_remove_cash(New_cash, cash):
+#     New_cash = get_total_cash - 10
+#     return cash
+    
+def get_pets_sold(sold):
+    return sold["admin"]["pets_sold"]
+
+
+
+
+
 
 
 
