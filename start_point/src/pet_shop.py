408388b2299430c3294b8f74e50bfe05d9c25b8b
Jamie <jamienolan93@hotmail.co.uk>
2021-11-21 18:07:09 +0000

    tried again and got number 4

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -4,16 +4,12 @@ def get_pet_shop_name(shop):
 def get_total_cash(total_cash):
     return total_cash["admin"]["total_cash"]
 
-# def add_or_remove_cash(New_cash, cash):
-#     New_cash = get_total_cash + 10
-#     return cash
-
-# def add_or_remove_cash(New_cash, cash):
-#     New_cash = get_total_cash - 10
-#     return cash
+def add_or_remove_cash(total_cash, value_integer):
+    total_cash["admin"]["total_cash"] += value_integer
+    return total_cash["admin"]["total_cash"]
     
-def get_pets_sold(sold):
-    return sold["admin"]["pets_sold"]
+
+    
 
 
 
