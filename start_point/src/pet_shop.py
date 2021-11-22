f009d7dec11ce01f690d8a8e09d8aa1fc957fe68
Jamie <jamienolan93@hotmail.co.uk>
2021-11-22 07:33:31 +0000

    extensions

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -54,7 +54,10 @@ def get_customer_pet_count(num_pets):
 def add_pet_to_customer(customers, new):
     return customers["pets"].append(new)
 
-    
+def customer_can_afford_pet(customer, new_pet):
+    if customer["cash"] >= new_pet["price"]:
+        return True
+    return False     
 
 
 
